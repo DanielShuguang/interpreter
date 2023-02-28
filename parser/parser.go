f841e3a2b2486c6ca8b05afcd15861f5174a0374
DanielShuguang <41940314+DanielShuguang@users.noreply.github.com>
2023-02-28 13:47:30 +0800

    feat: return 语句的解析；追加部分注释

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"monkey-compiler-study/ast"
 	"monkey-compiler-study/lexer"
 	"monkey-compiler-study/token"
@@ -11,16 +12,32 @@ type Parser struct {
 
 	curToken  token.Token // 当前字符的信息
 	peekToken token.Token // 下一个字符的信息
+
+	errors []string // 检测到的语法错误列表
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// 读取两个词法单元，以设置 curToken 和 peekToken
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+// 往程序错误信息中插入一条错误
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 // 同时移动 curToken 和 peekToken
@@ -31,29 +48,33 @@ func (p *Parser) nextToken() {
 
 func (p *Parser) ParserProgram() *ast.Program {
 	program := &ast.Program{}
-
 	program.Statements = []ast.Statement{}
 
-	for p.curToken.Type != token.EOF {
+	// 没有遇到结束符号时需要一直读取程序代码
+	for !p.curTokenIs(token.EOF) {
 		stmt := p.parseStatement()
 		if stmt != nil {
 			program.Statements = append(program.Statements, stmt)
-			p.nextToken()
 		}
+		p.nextToken()
 	}
 
 	return program
 }
 
+// 获取当前语句的准确类型
 func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
-		return p.parseLetStatement()
+		return p.parseLetStatement() // 遇到 let 关键词，判断是否为正确的声明语法
+	case token.RETURN:
+		return p.parseReturnStatement() // 遇到 return 关键词，判断是否为正确的返回语句
 	default:
 		return nil
 	}
 }
 
+// 遇到 let 关键词时，继续向后窥探，判断是否为完整的声明语句
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
 
@@ -78,19 +99,38 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	return stmt
 }
 
+// 遇到 return 关键词时，继续向后窥探，判断是否为完整的语句
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: 跳过对表达式的处理，知道遇见分号
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// 当前token是否为某一类型
 func (p *Parser) curTokenIs(t token.TokenType) bool {
 	return p.curToken.Type == t
 }
 
+// 下一个token是否为某一类型
 func (p *Parser) peekTokenIs(t token.TokenType) bool {
 	return p.peekToken.Type == t
 }
 
+// 下一个token是否为期望类型
 func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
 	} else {
+		// 不是预期类型，插入一条报错信息
+		p.peekError(t)
 		return false
 	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -17,6 +17,7 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParserProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParserProgram() returned nil")
 	}
@@ -39,6 +40,52 @@ func TestLetStatements(t *testing.T) {
 			return
 		}
 	}
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+	return 5;
+	return 10;
+	return 993 322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParserProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+				returnStmt.TokenLiteral())
+		}
+	}
+}
+
+// 检查parser中生成的语法错误
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
