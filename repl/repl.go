fce1983a62e83c8336cafdb7d31595e6d60d0181
DanielShuguang <41940314+DanielShuguang@users.noreply.github.com>
2023-08-13 00:39:05 +0800

    feat: 绑定与环境

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"monkey-compiler-study/evaluator"
 	"monkey-compiler-study/lexer"
+	"monkey-compiler-study/object"
 	"monkey-compiler-study/parser"
 )
 
@@ -13,6 +14,7 @@ const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
